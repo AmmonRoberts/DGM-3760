8af6f1db3fa1874132351a22b33681c14cfb7b40
Ammon Roberts <ammoneroberts@gmail.com>
2021-08-24 23:15:59 -0600

    Added truncate, prepend, curtail and concat

--- js-assessment/app/arrays.js
+++ js-assessment/app/arrays.js
@@ -37,19 +37,22 @@ exports.arraysAnswers = {
   },
 
   truncate: function(arr) {
-
+    arr.pop(arr.length - 1);
+    return arr;
   },
 
   prepend: function(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail: function(arr) {
-
+    arr.shift();
+    return arr;
   },
 
   concat: function(arr1, arr2) {
-
+    return [...arr1, ...arr2];
   },
 
   insert: function(arr, item, index) {
